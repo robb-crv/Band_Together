d2bde64b7abf6ac2b3d1920fd014422268fda470
Roberto_Caravelli <roberto.caravelli@gmail.com>
2017-05-08 12:06:21 +0200

    pages home-about-contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contacts'
+
   #devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
---
+++ spec/views/static_pages/contacts.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "static_pages/contacts.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
