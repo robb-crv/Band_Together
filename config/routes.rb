ab82fb421196c2eb501e4b91812ff437b410e523
Acidevil <coluzziandreajh@gmail.com>
2017-05-10 10:43:58 +0000

    facebook oauth

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
   get 'static_pages/contacts'
 
 #devise_for :users
-<<<<<<< HEAD
+
   devise_for :users, controllers: { sessions: 'users/sessions', 
-  		:omniauth_callbacks => "users/omniauth_callbacks" }
-=======
-  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks"  }
->>>>>>> mioBranch
+  									:omniauth_callbacks => "users/omniauth_callbacks" }  
+
+#Route che serve nella form di signup per far apparire nella select della selezione della regione la lista delle regioni appartenenti alla nazione scelta dall'utente
+  get '/states/:nation', to: 'application#states'
   
 end
