873cd4576b965b9d31736ac2cf95999c34c3edae
Roberto_Caravelli <roberto.caravelli@gmail.com>
2017-05-18 15:53:48 +0200

    final fix int test

--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -237,6 +237,11 @@ Then /^I should be on the User Profile Page$/ do
   expect(page.current_path).to eq users_show_path
 end
 
+When /^I follow "Profile"$/ do 
+ 
+  find(:css, '#Profile').click
+end
+
 And /^I should see "PROFILO UTENTE"$/ do
   expect(page).to have_content "Profilo utente"
 end
