33dfc6a1ead490d5af0bc30c34f3b949bbf201a6
RezeR93 <xael93@gmail.com>
2017-06-07 14:16:53 +0200

    Fix cancan

--- app/controllers/advertisment_controller.rb
+++ app/controllers/advertisment_controller.rb
@@ -1,7 +1,5 @@
 class AdvertismentController < ApplicationController
 
-	load_and_authorize_resource
-
 	before_action :authenticate_user!
 
 	def index
--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,7 +1,5 @@
 class BandController < ApplicationController
 
-	load_and_authorize_resource
-
 	before_action :authenticate_user!
 
 	def index
