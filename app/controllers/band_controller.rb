8b290cc9310aef8cbfc83f0da363dfdbb4d5a3e5
andrea <coluzziandreajh@gmail.com>
2017-06-08 13:40:22 +0200

    added error messages on create and edit

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -7,6 +7,7 @@ class BandController < ApplicationController
 	end
 
 	def new
+		@band = Band.new()
 	end
 
 	def create
@@ -20,8 +21,8 @@ class BandController < ApplicationController
 			flash[:success] = "The band has been created successfully."
 		else
 
-			redirect_to band_new_path
-			flash[:danger] = "Invalid parameters."
+			render band_new_path
+			#flash[:danger] = "Invalid parameters."
 		end
 	end
 
@@ -41,8 +42,16 @@ class BandController < ApplicationController
 
 	def update
 		@band= Band.find(params[:id])
-		@band.update(band_params)
-		redirect_to band_index_path(:id => @band.band_manager)
+		updated = @band.update(band_params)
+
+
+		if updated
+			redirect_to band_index_path(:id => @band.band_manager)
+			flash[:success] = "Successfully updated informations."
+		else
+			render band_edit_path
+		#  flash[:danger] = "Invalid parameters : " + user.errors.full_messages
+		end
 	end
 
 	def destroy
@@ -61,7 +70,7 @@ class BandController < ApplicationController
 
 	private
   	def band_params()
-    	params.require(:band).permit(:name, :description, :musical_genre, :band_manager_id, :nation , :region, :city)
+    	params.require(:band).permit(:name, :description, :musical_genre_id, :band_manager_id, :nation , :region, :city)
    	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,12 +34,13 @@ Rails.application.routes.draw do
   post 'advertisment/destroy'
 
   delete 'advertisment/destroy'
-  
+
   post 'advertisment/update'
 
   get 'advertisment/edit'
 
-  
+
+
   # Routes per le band
 
   get 'band/new'
