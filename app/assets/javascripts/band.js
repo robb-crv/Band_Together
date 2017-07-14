b0f60f9a4d7c443508ab8ed826e4e897fb9b3ef6
Andrea Coluzzi <coluzziandreajh@gmail.com>
2017-07-14 19:23:49 +0200

    share on facebook

--- app/assets/javascripts/band.js
+++ app/assets/javascripts/band.js
@@ -27,4 +27,9 @@ document.addEventListener("turbolinks:load", function() {
 
      );
 
+
+    $("#share_on_fb").click(function(){
+         window.open('https://www.facebook.com/dialog/feed?app_id=625050674363213&quote=Just%20discovered%20an%20amazing%20band%20please%20join%20to%20Band%20Together%20and%20see%20similar%20bands%20and%20other%20musicians&link=https%3A%2F%2Fwww.facebook.com%2Fbandtogetherofficial&hashtag=%23BandTogether', 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    });
+
  });
